eb03e772ae1c686cefb796d61cdda0309fcbc01c
suhyeon <soo4833@naver.com>
2021-10-12 17:58:39 +0900

    비밀번호 설정관리

--- Study/models/Users.js
+++ Study/models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -11,7 +14,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 5
+        maxlength: 10
     },
     lastname: {
         type: String,
@@ -29,6 +32,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+//저장하기 전에 
+userSchema.pre('save', function(next){
+    //비밀번호를 암호화 시킨다.
+    var user = this;
+
+    //비밀번호를 바꿀 때만 암호화를 하기 위해서
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) { //hash는 암호화 된 비밀번호
+                if (err) return next(err)
+                user.password = bcrypt.hash
+                next()
+            })
+        })
+    }
+
+})
+
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
