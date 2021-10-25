f15f88fd734bdae8f325dc2a3bdd43adf0779594
suhyeon <soo4833@naver.com>
2021-10-25 23:52:27 +0900

    #12 로그인 기능 및 토큰 생성

--- Study/models/Users.js
+++ Study/models/Users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -49,10 +50,37 @@ userSchema.pre('save', function(next){
                 next()
             })
         })
+    } else {
+        next();
     }
-
 })
 
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 1234567 암호화된 패스워드 ~~~~~///둘이 같은지 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // ->
+
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
