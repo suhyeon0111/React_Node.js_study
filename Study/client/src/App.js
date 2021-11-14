cb991f8b407e3e41bf3d90d1fd37a3961627b22a
suhyeon <soo4833@naver.com>
2021-11-15 00:12:18 +0900

    [#21]: 데이터 flow&Axios

--- Study/client/src/App.js
+++ Study/client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+   BrowserRouter as Router, Routes, Route
+  } from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+
+import LoginPage from "./components/views/LoginPage/LoginPage";
+
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+return (
+  <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Routes>
+      </div>
+  </Router>
+
   );
 }
 
--- Study/client/src/components/views/LandingPage/LandingPage.js
+++ Study/client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
+import axios from 'axios';
 
 function LandingPage(){
+    
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <div>
-            LandingPage
+            <h1>LandingPage</h1>
         </div>
     )
 }
