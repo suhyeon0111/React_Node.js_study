5e1b1c831a6b9833260e61163e8f04d1c8ed4f25
suhyeon <soo4833@naver.com>
2021-11-30 23:43:07 +0900

    [#32]: 로그아웃

--- Study/client/src/components/views/LandingPage/LandingPage.js
+++ Study/client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,33 @@
 import React,{ useEffect } from 'react'
 import axios from 'axios';
 
-function LandingPage(){
+function LandingPage(props){
     
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
 
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push("/login")
+                } else {
+                    alert('로그아웃 하는데 실패했습니다.')
+                }
+                console.log(response.data)
+            })
+    }
     return (
         <div style={{
             display:'flex', justifyContent:'center', alignItems: 'center'
             ,width: '100%', height:'100vh'
         }}>
             <h1>시작 페이지</h1>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
